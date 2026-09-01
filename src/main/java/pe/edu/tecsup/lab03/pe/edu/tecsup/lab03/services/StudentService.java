3b472683c1dd5ea1ddb12ba9a98ae7131b87d13d
Jeff-amaro <jeff.amaro@tecsup.edu.pe>
2026-09-01 17:34:59 -0500

    "Actualizacion de la clase StudentService"

---
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/services/StudentService.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.services;
+//Amaro
+
+public class StudentService {
+    public void registrarEstudiante(String nombre) {
+        System.out.println("Registrando al estudiante ");
+    }
+}
