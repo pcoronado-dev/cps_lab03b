af9bd1b4adfb49e47772e6890b7c2e454a8f3914
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-01 16:25:31 -0500

    Agregar método main básico para pruebas

---
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
+
+public class StudentController {
+    public static void main(String[] args) {
+        System.out.println("StudentController");
+    }
+}
