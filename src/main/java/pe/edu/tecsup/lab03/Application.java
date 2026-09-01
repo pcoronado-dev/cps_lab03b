8e30eb68dba1e7b8fb629ece7999f7fa618eab12
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-01 16:11:10 -0500

    first commit

--- src/main/java/pe/edu/tecsup/lab03/Application.java
+++ src/main/java/pe/edu/tecsup/lab03/Application.java
@@ -2,7 +2,6 @@ package pe.edu.tecsup.lab03;
 
 public class Application {
     public static void main(String[] args) {
-        // Your code here
         System.out.println("Application started");
     }
 }
