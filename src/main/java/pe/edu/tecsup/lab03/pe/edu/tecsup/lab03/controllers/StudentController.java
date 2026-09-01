3574f6a086ec87b9038a6f732a3bd5008bf08734
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-01 17:19:04 -0500

    Modificacion de StudentController

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,7 +1,5 @@
 package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
-    public static void main(String[] args) {
-        System.out.println("StudentController");
-    }
+    //PIERO ALEXANDER CORONADO QUISPE
 }
